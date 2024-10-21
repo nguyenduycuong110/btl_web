5d0590e93b86eefc172b992cbee9b2e9c6a8c066
nguyenduycuong110 <nguyenduycuongntb@gmail.com>
2024-10-21 19:28:24 +0700

    Second

--- wpbingosite.ht/resources/library.js
+++ wpbingosite.ht/resources/library.js
@@ -65,7 +65,7 @@
             breakpoints: {
                 // when window width is >= 320px
                 320: {
-                    slidesPerView: 2.5,
+                    slidesPerView: 2,
                     spaceBetween: 0
                 },
                 // when window width is >= 480px
